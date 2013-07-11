9ba6539e7c5a98f8951dd87a06287b37d6f5501d
graiola <student@lorien.pal-local>
2013-07-11 13:53:21 +0200

    changed the main.py, no compensations need anymore

--- sot_controller/python_code/main.py
+++ sot_controller/python_code/main.py
@@ -1,38 +1,9 @@
 from startup import *
 
-rospy.init_node('tf_reem')
-listener = tf.TransformListener()
-filename = "/tmp/out_from_py.txt"
-
-def transformation(frame_1,frame_2):
-    now = rospy.Time.now()
-    listener.waitForTransform(frame_1,frame_2, now, rospy.Duration(4.0))
-    (xyz,quat) = listener.lookupTransform(frame_1, frame_2, now)
-    return {'quat':quat,'xyz':xyz}
-
-def goalDef(frame_1,frame_2,xyz,quat):
-    R = quat2mat(quat)
+def goalDef(xyz,quat):
     goal = numpy.matrix([[0 , 0, 0, xyz[0]], [0,  0, 0, xyz[1]], [0, 0, 0, xyz[2]], [0, 0, 0, 1]])
-    cur_pose = transformation(frame_1,frame_2)
-    R_comp = quat2mat(cur_pose['quat'])
-    goal_r = R * R_comp.transpose()
+    goal_r = quat2mat(quat)
     goal[0:3,0:3] = goal_r
-    return goal
-
-def visDef(frame_1,frame_2,xyz):
-    cur_pose = transformation(frame_1,frame_2)
-    cur_xyz = cur_pose['xyz'] 
-    R_comp = quat2mat(cur_pose['quat'])
-    pos = numpy.array([(xyz - cur_xyz)]).T
-    axis = R_comp * pos
-    theta = math.atan2(axis[1],axis[0])
-    alpha = math.atan2(axis[2],axis[1])
-    Rz = rotz(-theta)
-    Rx = rotx(-alpha)
-    goal = numpy.matrix([[0 , 0, 0, cur_xyz[0]], [0,  0, 0, cur_xyz[1]], [0, 0, 0, cur_xyz[2]], [0, 0, 0, 1]])
-    goal_r = Rz * Rx
-    goal_r = goal_r * R_comp.transpose()
-    goal[0:3,0:3] = goal_r.transpose()
     return goal
 
 jointLimits_flag = 1
@@ -40,13 +11,13 @@ contact_waist_flag = 1
 gaze_flag = 1
 rw_flag = 1
 lw_flag = 0
-com_flag = 1
+com_eq_flag = 0
 
 quat = numpy.array([-0.377,-0.06,-0.142,0.91])
-xyz = numpy.array([0.35,-0.3, 0.9])
-goal_rw = goalDef("/torso_base_link","/arm_right_tool_link",xyz,quat)
-goal_lw = goalDef("/torso_base_link","/arm_left_tool_link",xyz,quat)
-goal_gz = visDef("/torso_base_link","/head_2_link",xyz)
+xyz = numpy.array([0.35,-0.3,1.25])
+
+goal_rw = goalDef(xyz,quat)
+goal_lw = goalDef(xyz,quat)
 
 taskRW = MetaTaskKine6d('rw',robot.dynamic,'right-wrist','right-wrist')
 taskRW.feature.frame('current')
@@ -54,8 +25,9 @@ taskRW.feature.frame('current')
 taskLW = MetaTaskKine6d('lw',robot.dynamic,'left-wrist','left-wrist')
 taskLW.feature.frame('current')
 
-taskGAZE = MetaTaskKine6d('gz',robot.dynamic,'gaze','gaze')
-taskGAZE.feature.frame('current')
+taskGAZE = MetaTaskVisualPoint('gz',robot.dynamic,'gaze','gaze')
+taskGAZE.gain.setConstant(1000)
+taskGAZE.featureDes.xy.value = (0,0)
 
 robot.dynamic.upperJl.recompute(0)
 robot.dynamic.lowerJl.recompute(0)
@@ -71,10 +43,23 @@ taskWT = MetaTaskKine6d('wt',robot.dynamic,'waist','waist')
 taskWT.feature.frame('desired')
 taskWT.gain.setConstant(1000)
 
-taskCOM = MetaTaskKineCom(robot.dynamic)
-robot.dynamic.com.recompute(0)
-taskCOM.featureDes.errorIN.value = robot.dynamic.com.value
-taskCOM.task.controlGain.value = 10
+if com_eq_flag:
+    taskCOM = MetaTaskKineCom(robot.dynamic)
+    robot.dynamic.com.recompute(0)
+    taskCOM.featureDes.errorIN.value = robot.dynamic.com.value
+    taskCOM.task.controlGain.value = 10 
+else:
+    featureCOM = FeatureGeneric('featureCom')
+    plug(robot.dynamic.com,featureCOM.errorIN)
+    plug(robot.dynamic.Jcom,featureCOM.jacobianIN)
+    taskCOM = TaskInequality('com')
+    taskCOM.add(featureCOM.name)
+    taskCOM.selec.value = '011'
+    taskCOM.referenceInf.value = (-0.23,-0.24,0)
+    taskCOM.referenceSup.value = (0.1,0.24,0)
+    taskCOM.dt.value = 0.001
+    robot.dynamic.com.recompute(0)
+    taskCOM.controlGain.value = 10
 
 if jointLimits_flag:
     push(taskJL)
@@ -91,26 +76,25 @@ if lw_flag:
 if gaze_flag:
     push(taskGAZE)
 
-if com_flag:
-    push(taskCOM)
+push(taskCOM)
 
-time.sleep(5)
+if gaze_flag:
+    taskGAZE.goto3D((xyz[0],xyz[1],xyz[2]))
 
 if rw_flag:
     gotoNd(taskRW,goal_rw,'111111',100)
 
 if lw_flag:
-    gotoNd(taskLW,goal_lw,'111000',100)
-
-if gaze_flag:
-    gotoNd(taskGAZE,goal_gz,'111000',10)
+    gotoNd(taskLW,goal_lw,'111111',100)
 
 time.sleep(15)
 
 err2file(taskRW,filename,"w")
 err2file(taskLW,filename,"a")
-err2file(taskGAZE,filename,"a")
+err2file(taskGAZE,filename,"a") 
   
+#Unused code:
+"""      
 out_file = open("/tmp/joints_limits","w")
 count = 0
 for elem in robot.device.state.value:
@@ -120,22 +104,12 @@ for elem in robot.device.state.value:
     out_file.write("Rank: " + str(count) +  " state value: " + str(elem) + " lower bound: " + str(l) + "\n")
     out_file.write("Rank: " + str(count) +  " state value: " + str(elem) + " upper bound: " + str(u) + "\n")  
 
-out_file.close()  
-  
-#Unused code:
-"""      
-count = 0
-for elem in robot.device.state.value:
-    u = robot.dynamic.upperJl.value[count]
-    l = robot.dynamic.lowerJl.value[count]
-    count = count + 1
-    if (elem < l):
-        out_file.write("Rank: " + str(count) +  " state value: " + str(elem) + " lower bound: " + str(l) + "\n")
-    if (elem > u):
-        out_file.write("Rank: " + str(count) +  " state value: " + str(elem) + " upper bound: " + str(u) + "\n")
-        
-robot.dynamic.com.recompute(robot.device.state.time)       
+out_file.close()     
 """
+
+
+
+
 
 
 
